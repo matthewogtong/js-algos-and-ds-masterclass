99fc2ef4655d19e6dcca70ac21b3b3cb6e1a4acb
matthewogtong <ogtongm@gmail.com>
2021-08-10 14:34:16 -0400

    refactored solution implementing multiple pointers pattern

--- 05-problem-solving-patterns/sumZero.js
+++ 05-problem-solving-patterns/sumZero.js
@@ -4,7 +4,7 @@
 // sumZero ([-2, 0, 1, 3]) returns undefined
 // sumZero ([1, 2, 3]) returns undefined
 
-// naive solution (too many iterations and presents a time complexity of ON^2)
+// naive solution (too many iterations and presents a time complexity of O(N^2))
 function sumZero(arr) {
     for(let i = 0; i < arr.length; i++) {
         for(let j = i+1; j < arr.length; j++) {
@@ -16,3 +16,25 @@ function sumZero(arr) {
 }
 
 console.log(sumZero([-3,-2,-1, 0, 1, 2, 3]))
+
+
+// refactored solution implementing multiple pointers pattern
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+
+function sumZero(arr) {
+     let left = 0
+     let right = arr.length - 1
+     while(left < right) {
+         let sum = arr[left] + arr[right]
+         if(sum === 0) {
+             return [arr[left], arr[right]]
+         } else if(sum > 0) {
+             right--
+         } else {
+             left++
+         }
+     }
+}
+
+console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 3, 10]))
