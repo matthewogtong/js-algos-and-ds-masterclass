5faf7af06cf03879f2467aac96792a3c61e50142
matthewogtong <ogtongm@gmail.com>
2021-08-10 23:34:18 -0400

    comment out naive solution for maxsubarraysum and confirm result with console log

--- 05-problem-solving-patterns/maxSubarraySum.js
+++ 05-problem-solving-patterns/maxSubarraySum.js
@@ -8,25 +8,25 @@ maxSubarraySum([1,2,5,2,8,1,5],2) // 10
 
 // naive solution
 
- function maxSubarraySum(arr, num) {
-     if(num > arr.length) {
-         return null
-     }
+//  function maxSubarraySum(arr, num) {
+//      if(num > arr.length) {
+//          return null
+//      }
 
-     let max = -Infinity
-     for(let i = 0; i < arr.length - num + 1; i++) {
-         let temp = 0
-        for(let j = 0; j < num; j++) {
-            temp += arr[i + j]
-        }
-        if(temp > max) {
-            max = temp;
-        }
-     }
-     return max;
- }
+//      let max = -Infinity
+//      for(let i = 0; i < arr.length - num + 1; i++) {
+//          let temp = 0
+//         for(let j = 0; j < num; j++) {
+//             temp += arr[i + j]
+//         }
+//         if(temp > max) {
+//             max = temp;
+//         }
+//      }
+//      return max;
+//  }
 
- console.log(maxSubarraySum([1,2,5,2,8,1,5],2))
+//  console.log(maxSubarraySum([1,2,5,2,8,1,5],2))
 
  // refactored solution implementing Sliding Window approach
  // Time Complexity - O(N)
@@ -46,3 +46,4 @@ maxSubarraySum([1,2,5,2,8,1,5],2) // 10
      return maxSum
  }
 
+ console.log(maxSubarraySum([1,2,5,2,8,1,5],2))
