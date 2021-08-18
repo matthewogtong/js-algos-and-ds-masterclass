7381c5dd666b7c8f002d79da799d869a74ad99d3
matthewogtong <ogtongm@gmail.com>
2021-08-17 21:04:28 -0400

    solve findLongestSubstring function

--- 05-problem-solving-patterns/validAnagram.js
+++ 05-problem-solving-patterns/validAnagram.js
@@ -28,29 +28,52 @@ function validAnagram(str1, str2) {
 
 console.log(validAnagram("hello", "heoll"))
 
-
-// second solution
-
-function validAnagram(first, second) {
-    if (first.length !== second.length) {
+function validAnagram(str1, str2) {
+    if (str1.length !== str2.length) {
         return false
     }
 
-    let lookup = {}
+    let fq1 = {}
+    let fq2 = {}
 
-    for (let i = 0; i < first.length; i++) {
-        lookup[first[i]] ? lookup[first[i]] += 1 : lookup[first[i]] = 1
+    for (let val of str1) {
+        fq[val] = (fq[val] || 0) + 1
     }
 
-    for (let i = 0; i < second.length; i++) {
-        if (!(lookup[second[i]])) {
+    for( let val of str2) {
+        fq[val] = (fq[val] || 0) + 1
+    }
+
+    for (let key in fq1) {
+        if (!(fq1[key] in fq) && !(fq1[key] === fq2[key])) {
             return false
-        } else {
-            lookup[i] -= 1
         }
     }
-
-    return true
 }
 
-console.log(validAnagram("testing", "tesingt"))
+
+// second solution
+
+// function validAnagram(first, second) {
+//     if (first.length !== second.length) {
+//         return false
+//     }
+
+//     let lookup = {}
+
+//     for (let i = 0; i < first.length; i++) {
+//         lookup[first[i]] ? lookup[first[i]] += 1 : lookup[first[i]] = 1
+//     }
+
+//     for (let i = 0; i < second.length; i++) {
+//         if (!(lookup[second[i]])) {
+//             return false
+//         } else {
+//             lookup[i] -= 1
+//         }
+//     }
+
+//     return true
+// }
+
+// console.log(validAnagram("testing", "tesingt"))
