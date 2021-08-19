8fcb9363a499fa65ee08dd3c33d34ade70437ba7
matthewogtong <ogtongm@gmail.com>
2021-08-19 18:06:53 -0400

    complete problem solving patterns

--- 06-section-challenges/maxSubarraySum.js
+++ 06-section-challenges/maxSubarraySum.js
@@ -41,9 +41,9 @@ maxSubarraySum([1,2,5,2,8,1,5],2) // 10
      tempSum = maxSum
      for(let i = num; i < arr.length; i++) {
          tempSum = tempSum - arr[i - num] + arr[i]
-         maxSum = Max.max(maxSum, tempSum)
+         maxSum = Math.max(maxSum, tempSum)
      }
      return maxSum
  }
 
- console.log(maxSubarraySum([1,2,5,2,8,1,5],2))
+ console.log(maxSubarraySum([2,6,9,2,1,8,5,6,3],3))
